faf68844f558bc50760a563e8e8ad6ac0dbf4d7b
yonathanm10 <yonathanm@cocoindonesia.com>
2023-10-25 09:50:34 +0700

    feat(post): showing number of votes in post, for path operation get_posts and get_post

--- app/routers/post.py
+++ app/routers/post.py
@@ -3,16 +3,19 @@ from fastapi import Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from ..database import get_db
 from typing import List, Optional
+from sqlalchemy import func
 
 router = APIRouter(
     prefix='/posts',
     tags = ['Post']
 )
 
-@router.get('/', response_model=List[schemas.Post])
+@router.get('/', response_model=List[schemas.PostOut])
 def get_posts(db:Session = Depends(get_db), current_user=Depends(oauth2.get_current_user),
               limit:int=10, skip:int=0, search:Optional[str]=""):
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    posts = db.query(models.Post,func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote,models.Vote.post_id==models.Post.id,isouter=True).group_by(models.Post.id).filter(
+            models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
@@ -23,9 +26,10 @@ def create_posts(post:schemas.PostCreate, db:Session = Depends(get_db), current_
     db.refresh(new_post)
     return new_post
 
-@router.get('/{id}', response_model=schemas.Post)
+@router.get('/{id}', response_model=schemas.PostOut)
 def get_post(id:int, db:Session = Depends(get_db), current_user=Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id==id).first()
+    post = db.query(models.Post,func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote,models.Vote.post_id==models.Post.id,isouter=True).group_by(models.Post.id).filter(models.Post.id==id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with {id} was not found")
     return post

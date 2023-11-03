3dfb58e706feb4d2158725500776a01c6da58eed
yonathan <yonathanm@cocoindonesia.com>
2023-11-03 18:09:21 +0700

    fix(user): fix the double same name of path operation function edit path operation function name for /{id} as get_user

--- app/routers/user.py
+++ app/routers/user.py
@@ -22,7 +22,7 @@ def create_user(user:schemas.UserCreate, db:Session = Depends(get_db)):
     return new_user
 
 @router.get('/{id}',  status_code=status.HTTP_200_OK, response_model=schemas.UserOut)
-def create_user(id:int, db:Session = Depends(get_db)):
+def get_user(id:int, db:Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id==id).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'user with id: {id} does not exist')
